8bc54f7b27173a232426a3193dd58d755747f2b7
Tobias Boege <tboege@ovgu.de>
2020-09-17 15:02:07 +0200

    Document Cube and fix some bugs
    Finish Cube documentation. Also rename ->δ and ->Δ to one method ->ci
    to not force users to use utf8. Fix bugs in those methods which came
    from a change in the way 2-faces are stored in this module, compared
    to the one these implementations were copied from.

--- lib/CInet/Cube.pm
+++ lib/CInet/Cube.pm
@@ -1,3 +1,23 @@
+=encoding utf8
+
+=head1 NAME
+
+CInet::Cube - The ground set of a CInet::Relation
+
+=head1 SYNOPSIS
+
+    my $cube = CUBE(5);  # ground set 1..5
+
+    # Print the permuted ordering of 2-faces.
+    my $p = [4,1,3,5,2];
+    for my $ijK ($cube->squares) {
+        my $pijK = $cube->permute($p => $ijK);
+        say $cube->pack($ijK), ' -> ', $cube->pack($pijK);
+    }
+
+=cut
+
+# ABSTRACT: The ground set of a CInet::Relation
 package CInet::Cube;
 
 use utf8;
@@ -13,18 +33,31 @@ use Array::Set qw(set_union set_intersect set_diff set_symdiff);
 use CInet::Hash::FaceKey;
 use CInet::Imset;
 
-# The CUBE sub keeps one instance per ground set around.
-tie my %CUBES, 'CInet::Hash::FaceKey';
+=head1 DESCRIPTION
 
-sub CUBE :Export(:DEFAULT) {
-    my $N = _make_set(@_);
-    $CUBES{[$N, []]} //= __PACKAGE__->new(@_)
-}
 
+
+=cut
+
+# Process the first argument into an arrayref as CInet::Cube->new requires.
 sub _make_set {
     my $argref = reftype($_[0]);
     my $N = $argref && $argref eq 'ARRAY' ? $_[0] : [1 .. $_[0]];
 }
+
+=head2 Methods
+
+=head3 new
+
+    my $cube = CInet::Cube->new($n);
+    my $cube = CInet::Cube->new($set);
+
+Create a new cube object on the given ground set. If the argument is an
+arrayref C<$set>, then that is the ground set. The caller is responsible
+for ensuring that it is properly sorted and deduplicated. Otherwise the
+ground set is taken to be C<1 .. $n>.
+
+=cut
 
 sub new {
     my $class = shift;
@@ -32,6 +65,8 @@ sub new {
     bless { set => $N, dim => 0+ @$N }, $class
 }
 
+# Reify the array and lookup structure of $k-dimensional faces in the
+# invocant and return the array.
 sub _make_faces {
     my ($self, $k) = @_;
     my $N = $self->{set};
@@ -57,13 +92,47 @@ sub _make_faces {
     @faces
 }
 
+=head3 dim
+
+    my $dim = $cube->dim;
+
+Returns the dimension, that is the size of the ground set, of this cube.
+
+=cut
+
 sub dim {
     shift->{dim}
 }
 
+=head3 set
+
+    my $set = $cube->set;
+
+Returns the ground set of this cube, which are axis labels.
+
+=cut
+
 sub set {
     shift->{set}
 }
+
+=head3 faces, vertices, edges, squares
+
+    my @all_faces  = $cube->faces;
+    my @some_faces = $cube->faces(0, 2, $cube->dim - 1);
+
+Returns faces of the cube. If no arguments are given, all faces are returned
+in order of ascending dimension and each fixed-dimensional slice in its
+proper order.
+
+When specific dimensions are asked for in the arguments to this method, only
+these faces are returned.
+
+Each cube object computes the array of d-dimensional faces only on demand.
+Once computed, the array is cached. Other methods besides L<#faces> which
+can cause the face array to be reified are L<#pack> and L<#unpack>.
+
+=cut
 
 sub faces {
     my $self = shift;
@@ -75,6 +144,19 @@ sub faces {
     }
     @res
 }
+
+=pod
+
+There are predefined accessors for faces which are often used:
+
+    my @vertices = $cube->vertices;  # ->faces(0)
+    my @edges    = $cube->edges;     # ->faces(1)
+    my @squares  = $cube->squares;   # ->faces(2)
+
+These methods return, respectively, the zero-, one- and two-dimensional
+faces.
+
+=cut
 
 sub vertices {
     shift->faces(0)
@@ -88,6 +170,16 @@ sub squares {
     shift->faces(2)
 }
 
+=head3 pack
+
+    my $nr = $cube->pack($face);
+
+Maps a (d-dimensional) face to its 1-based position in the proper ordering
+of d-dimensional faces of the cube. If the face was not found, an exception
+is thrown.
+
+=cut
+
 sub pack {
     my ($self, $IK) = @_;
     my $d = $IK->[0]->@*;
@@ -99,12 +191,35 @@ sub pack {
         ]} over ground set @{[ join('', $self->{set}->@*) ]}";
 }
 
+=head3 unpack
+
+    my $face = $cube->unpack($d => $nr);
+
+The opposite of the L<#pack> method, takes a dimension and 1-based position
+number and returns the corresponding face object, or dies.
+
+=cut
+
 sub unpack {
     my ($self, $d, $v) = @_;
     $self->_make_faces($d) unless defined $self->{faces}->[$d];
     $self->{faces}->[$d]->[$v] //
         confess "lookup failed on $d/$v";
 }
+
+=head3 permute
+
+    my $pface = $cube->permute($p => $face);
+
+The symmetric group on C<< $cube->set >> acts on the axes of the cube
+and gives rise to I<permutation symmetries> of the face lattice. Given
+a permutation and a face, this method returns the corresponding face
+on the permuted cube.
+
+The permutation is given in "one-line notation", that is the i-th entry
+gives the image of the i-th element of the ground set.
+
+=cut
 
 # Convert a list of ground set elements to their 0-based indices into
 # the ground set.
@@ -130,6 +245,17 @@ sub permute {
     ]
 }
 
+=head3 dual
+
+    my $dface = $cube->dual($face);
+
+The cube has a distinguished symmetry by which all mirror symmetries are
+applied simultaneously. This is referred to as I<duality>.
+
+This is the same as calling L<#swap> with C<< $Z = $cube->set >>.
+
+=cut
+
 sub dual {
     my ($self, $IK) = @_;
     my ($I, $K) = $IK->@*;
@@ -137,27 +263,111 @@ sub dual {
     [ $I, set_diff($N, set_union($I, $K)) ]
 }
 
+=head3 swap
+
+    my $Zface = $cube->swap($Z => $face);
+
+For each subset C<$Z> of the axes of the cube, there is a mirror symmetry
+which applies simultaneously all the reflections orthogonal to each axis
+in C<$Z>. This is called I<swapping> and it generalizes the operation of
+L<#dual>.
+
+=cut
+
 sub swap {
     my ($self, $Z, $IK) = @_;
     my ($I, $K) = $IK->@*;
     [ $I, set_symdiff($K, set_diff($Z, $I)) ]
 }
 
+=head3 h
+
+    my $h = $cube->h($unit);
+
+Creates a zero or unit L<CInet::Imset> on this cube. The C<$unit> argument
+is optional but if given must be a subset of the ground set. This coordinate
+of the returned imset is set to 1, all others to 0.
+
+The C<$unit> arrayref is automatically deduplicated.
+
+=cut
+
 sub h {
     my $self = shift;
-    CInet::Imset->new($self => [uniq shift->@*])
+    my $unit = shift // [];
+    CInet::Imset->new($self => [uniq $unit->@*])
 }
 
-sub δ {
-    my $self = shift;
-    my ($i, $j, @K) = shift->@*;
-    $self->h([$i,@K]) + $self->h([$j,@K]) - $self->h([$i,$j,@K]) - $self->h([@K])
-}
+=head3 ci
 
-sub Δ {
+    my $Δ = $cube->ci($I, $J);
+    my $δ = $cube->ci($ijK);
+
+Each pair of sets C<$I> and C<$J> defines an instance of the submodular
+inequality on an imset whose domain is C<$cube>, namely
+
+    h(I) + h(J) - h(I ∪ J) - h(I ∩ J) ≥ 0
+
+This is an inequality over a linear functional on the imset and the
+C<< -> ci >> method returns the imset of coefficients of this functional.
+Matúš uses the notation C<< Δ_{I,J} >> for this and Studený calls it
+a semielementary imset.
+
+When a single argument 2-face C<$ijK> is passed, the corresponding
+elementary imset is returned, where C<I = i ∪ K> and C<J = j ∪ K>.
+
+This method is called "ci" because equality in the submodular inequality
+(so being on the hyperplane orthogonal to the returned imset) for an imset
+which is proportional to an entropy vector characterizes conditional
+independence in the backing probability distribution.
+
+=cut
+
+sub ci {
     my $self = shift;
     my ($I, $J) = @_;
+    if (not defined $J) {
+        my $ijK = $I;
+        $I = set_union([$ijK->[0]->[0]], $ijK->[1]);
+        $J = set_union([$ijK->[0]->[1]], $ijK->[1]);
+    }
     $self->h($I) + $self->h($J) - $self->h(set_union($I, $J)) - $self->h(set_intersect($I, $J))
 }
+
+=head2 Exports
+
+=head3 CUBE :Export(:DEFAULT)
+
+    my $cube = CUBE($n);
+    my $cube = CUBE($set);
+
+This is a shorthand for the C<< CInet::Cube->new >> constructor, but it also
+keeps a cache of cube objects indexed by ground sets. Prefer this function
+for getting cubes that you often use.
+
+This sub is exported by default.
+
+=cut
+
+# The CUBE sub keeps one instance per ground set around.
+tie my %CUBES, 'CInet::Hash::FaceKey';
+
+sub CUBE :Export(:DEFAULT) {
+    my $N = _make_set(@_);
+    $CUBES{[$N, []]} //= __PACKAGE__->new(@_)
+}
+
+=head1 AUTHOR
+
+Tobias Boege <tobs@taboege.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (C) 2020 by Tobias Boege.
+
+This is free software; you can redistribute it and/or
+modify it under the terms of the Artistic License 2.0.
+
+=cut
 
 ":wq"
