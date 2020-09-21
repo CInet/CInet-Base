6301c7fd120f36df5692e5aeea43b27a16a3d716
Tobias Boege <tboege@ovgu.de>
2020-09-21 14:31:30 +0200

    Fix Seq::Uniq
    It contained left-over copy-pastes.

--- lib/CInet/Seq/Uniq.pm
+++ lib/CInet/Seq/Uniq.pm
@@ -16,15 +16,7 @@ sub new {
 }
 
 sub next {
-    my ($prev, $code) = shift->@{'prev', 'code'};
-    while (1) {
-        my $x = $prev->next;
-        return undef if not defined $x;
-        local $_ = $x;
-        return $x if $code->($x);
-    }
     my ($prev, $code, $seen) = shift->@{'prev', 'code', 'seen'};
-    # Hash contains full orbits of every relation that we returned.
     while (defined(my $x = $prev->next)) {
         local $_ = $x;
         my $str = $code->($x);
